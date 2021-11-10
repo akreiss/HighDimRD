dd02eb3fdde086ef9c21fa185557e564c5c49044
Alexander Kreiss <akreiss@gmx.de>
2021-11-10 12:03:54 +0000

    Added RDHonest

--- R/main_functions.R
+++ R/main_functions.R
@@ -1,10 +1,10 @@
 #' High-Dimensional Covariates in RDD Estimation
-#' 
-#' 
+#'
+#'
 #' \code{HighDim_rd} function computes the treatment effect estimator in an RD
 #' setting when a large list of covariates is supplied by the user.
 #'
-#'     
+#'
 #' For the exact functionality of \code{HighDim_rd}, we refer to Kreiss and
 #' Rothe (2021). The vectors \code{Y},\code{X} contain the responses and the
 #' running variable, respectively, that is, an individual is exposed to the
@@ -27,6 +27,11 @@
 #'   covariate).
 #' @param c Cutoff point for treatment, individuals with X>=c are treated,
 #'   default is c=0.
+#' @param rd Sets which RD functions should be used to find bandwidths,
+#'   estimators and confidence sets. If "honest", then the RDHonest package is
+#'   used. If "robust", then the rdrobust package is used.
+#' @param niveau Niveau of the computed confidence set, between 0 and 1. Default
+#'   is 0.95.
 #' @param b Reference bandwidth used for doing the model selection step. If
 #'   b=NULL, the default, bandwidth selection from rdrobust without covariates
 #'   is used to find b. The final bandwidth used for model selection is given by
@@ -48,69 +53,68 @@
 #' @param kernel Specifies which kernel to be used for both model selection and
 #'   estimation, possible choices are triangular (the default), epanechnikov,
 #'   uniform.
-#' @param alpha,M,L Parameters for "LV" method for tuning parameter choice, ignored
-#'   if tpc!="LV".
+#' @param alpha,M,L Parameters for "LV" method for tuning parameter choice,
+#'   ignored if tpc!="LV".
+#' @param My,My_tilde,sclass Parameter which are used for RDHonest: My and
+#'   My_tilde are the bounds in the smoothness classe specified in sclass. The
+#'   default sclass is T, see RDHonest for details. My and My_tilde must be
+#'   specified by the user.
 #' @param OPC Parameter for "OPC" method for tuning parameter choice, ignored if
 #'   tpc!="OPC".
 #'
-#' @return List with five elements:
-#'     \tabular{ll}{
-#'     \code{rd} \tab Output from rdrobust using the selected covariates, see
-#'     rdrobust for a description of the output, the estimator can be found in
-#'     rd$Estimate. \cr
-#'     \code{covs} \tab Vector of selected covariate indices. \cr
-#'     \code{b} \tab Bandwidth used for model selection (as returned from
-#'     rdbwselect without covariates if b=NULL). \cr
-#'     \code{h} \tab Bandwidth used for the estimation (as returned from
-#'     rdbwselect with the selected covariates if h=NULL). \cr
-#'     \code{Z} \tab pars Vector of length p containing the estimated parameters
-#'     of the covariates.}
+#' @return List with five elements: \tabular{ll}{ \code{rd} \tab Output from
+#'   rdrobust using the selected covariates, see rdrobust for a description of
+#'   the output, the estimator can be found in rd$Estimate. \cr \code{covs} \tab
+#'   Vector of selected covariate indices. \cr \code{b} \tab Bandwidth used for
+#'   model selection (as returned from rdbwselect without covariates if b=NULL).
+#'   \cr \code{h} \tab Bandwidth used for the estimation (as returned from
+#'   rdbwselect with the selected covariates if h=NULL). \cr \code{Z} \tab pars
+#'   Vector of length p containing the estimated parameters of the covariates.}
 #'
 #'
-#' @section Authors:
-#' Alexander Kreiss, LSE, United Kingdom. \email{a.kreiss@@lse.ac.uk}
-#' 
-#' Christoph Rothe, University of Mannheim, Germany
+#' @section Authors: Alexander Kreiss, LSE, United Kingdom.
+#'   \email{a.kreiss@@lse.ac.uk}
 #'
-#' @section References: 
-#' Belloni, A., Chernozhukov, V. and Hansen, C. (2013): Inference on treatment
-#' effects after selection among high-dimensional controls. The Review of
-#' Economic Studies, 81, 608-650
+#'   Christoph Rothe, University of Mannheim, Germany
 #'
-#' Kreiss, A. and Rothe, C. (2021): Inference in Regression Discontinuity
-#' Designs with High-Dimensional Covariates. arxiv 2110.13725
+#' @section References: Belloni, A., Chernozhukov, V. and Hansen, C. (2013):
+#'   Inference on treatment effects after selection among high-dimensional
+#'   controls. The Review of Economic Studies, 81, 608-650
 #'
-#' Lederer, J. and Vogt, M. (2020): Estimating the Lasso's Effective Noise. arxiv
-#' 2004.11554
-#' 
+#'   Kreiss, A. and Rothe, C. (2021): Inference in Regression Discontinuity
+#'   Designs with High-Dimensional Covariates. arxiv 2110.13725
+#'
+#'   Lederer, J. and Vogt, M. (2020): Estimating the Lasso's Effective Noise.
+#'   arxiv 2004.11554
+#'
 #' @examples \dontrun{library(mvtnorm)
-#' 
+#'
 #' set.seed(12345)
-#' 
+#'
 #' n <- 1000
 #' sigma_y <- 0.1295
 #' sigma_z <- 0.1353
-#' 
+#'
 #' ## Running Variable
 #' x <- 2*rbeta(n,2,4)-1
-#' 
+#'
 #' ## Covariates
 #' Z <- rmvnorm(n,rep(0,5),sigma=diag(sigma_z^2,5))
-#' 
+#'
 #' ## Noise
 #' epsilon <- rnorm(n,mean=0,sd=sigma_y)
-#' 
+#'
 #' ## Treatment Indicator
 #' T <- as.numeric(x>=0)
-#' 
+#'
 #' ## Outcome
 #' Y <- (1-T)*(0.36+0.96*x+5.47*x^2+15.28*x^3+15.87*x^4+5.14*x^5+0.22*Z[,1])+
 #' T *(0.38+0.62*x-2.84*x^2+ 8.42*x^3-10.24*x^4+4.31*x^5+0.28*Z[,1])+epsilon
-#' 
+#'
 #' ## High-Dimensional Covariates
 #' Z1 <- fourier_basis(Z,4)
 #' Z_HighDim <- cbind(Z,interaction_terms(Z),Z1,interaction_terms(Z1))
-#' 
+#'
 #' ## Compute estimate with model selection
 #' cov_rd_CV  <- HighDim_rd(Y,x,Z_HighDim,tpc="CV")
 #' cov_rd_BCH <- HighDim_rd(Y,x,Z_HighDim,tpc="BCH")
@@ -119,33 +123,39 @@
 #' @seealso \code{\link{fourier_basis}}, \code{\link{interaction_terms}},
 #'   \code{\link{cross_interactions}}
 #' @export
-HighDim_rd <- function(Y,X,Z,c=0,b=NULL,bfactor=1,h=NULL,tpc,kernel="triangular",alpha=0.05,M=NULL,L=100,OPC=50) {
+HighDim_rd <- function(Y,X,Z,c=0,rd="robust",niveau=0.95,b=NULL,bfactor=1,h=NULL,tpc,kernel="triangular",alpha=0.05,M=NULL,L=100,OPC=50,My,My_tilde,sclass="T") {
   p <- dim(Z)[2]
   n <- length(Y)
-  
+
   ## Set Treatment Status
   T <- as.numeric(X>=c)
-  
+
   ## Chose the correct kernel function
   if(kernel=="epanechnikov") {
     kernel_fcn <- epanechnikov
   } else if(kernel=="triangular") {
     kernel_fcn <- triangular
   } else if(kernel=="uniform") {
-    kernel_fcn <- triangular
+    kernel_fcn <- uniform
   }
-  
+
   ## Step 1: Find Bandwidth
   ## If not specified, find bandwidth in RD without covariates
   if(is.null(b)==TRUE) {
-    b <- rdrobust::rdbwselect(Y,X,c=c,bwselect="mserd",kernel=kernel)
+    if(rd=="robust") {
+      bout <- rdrobust::rdbwselect(Y,X,c=c,bwselect="mserd",kernel=kernel)
+      b <- bout$bws[1]
+    } else {
+      bout <- RDHonest::RDOptBW(Y~X,cutoff=c,M=My,kern=kernel,opt.criterion="FLCI",bw.equal=TRUE,alpha=1-niveau,sclass=sclass,order=1)
+      b <- bout$h[1]
+    }
   }
-  
+
   ## Step 2: Model Selection
   ## Compute kernel weights
-  kernel_factor <- kernel_fcn((X-c)/(bfactor*b$bws[1]))/(bfactor*b$bws[1])
+  kernel_factor <- kernel_fcn((X-c)/(bfactor*b))/(bfactor*b)
   relevant_indices <- which(kernel_factor>0)
-  
+
   ## Transform Data
   T     <- X>=c
   Ymod  <- Y[relevant_indices]
@@ -153,11 +163,11 @@ HighDim_rd <- function(Y,X,Z,c=0,b=NULL,bfactor=1,h=NULL,tpc,kernel="triangular"
   Xmod  <- X[relevant_indices]-c
   TXmod <- T[relevant_indices]*(X[relevant_indices]-c)
   Zmod  <- Z[relevant_indices,]
-  
+
   ## Compute Centering and weights
   mu <- 1/n*colSums(Z[relevant_indices,]*matrix(rep(kernel_factor[relevant_indices],p),ncol=p))
-  w <- bfactor*b$bws[1]/n*colSums((Z[relevant_indices,]*matrix(rep(kernel_factor[relevant_indices],p),ncol=p)-matrix(rep(mu,length(relevant_indices)),ncol=p,byrow=TRUE))^2)
-  
+  w <- bfactor*b/n*colSums((Z[relevant_indices,]*matrix(rep(kernel_factor[relevant_indices],p),ncol=p)-matrix(rep(mu,length(relevant_indices)),ncol=p,byrow=TRUE))^2)
+
   ## Regress Covariates on Outcome weighted by the kernel
   if(tpc=="CV") {
     ## Do Cross-Validation
@@ -190,53 +200,75 @@ HighDim_rd <- function(Y,X,Z,c=0,b=NULL,bfactor=1,h=NULL,tpc,kernel="triangular"
     stop("No tuning parameter choice method specified")
   }
   sig_cov <- which(abs(Z_pars)>0)
-  
+
   ## Step 3: Do RD with chosen covariates
   ## Compute bandwidth if necessary
   if(is.null(h)==TRUE) {
     if(length(sig_cov)==0) {
       h <- b
     } else {
-      h <- rdrobust::rdbwselect(Y,X,covs=Z[,sig_cov],c=c,bwselect="mserd",kernel=kernel)
+      if(rd=="robust") {
+        hout <- rdrobust::rdbwselect(Y,X,covs=Z[,sig_cov],c=c,bwselect="mserd",kernel=kernel)
+        h <- hout$bws[1]
+      } else {
+        ## Adjust for Covariate Influence
+        cov_fit <- lm(Z[,sig_cov]~1+X+T+X*T,weights=kernel_factor)
+        v <- cov_fit$residuals
+        Sigma22 <- t(v)%*%(v*kernel_factor)/n
+        Sigma21 <- colSums(v*(kernel_factor*Y))/n
+        Sigma <- solve(Sigma22)%*%Sigma21
+        Ytilde <- Y-Z[,sig_cov]%*%Sigma
+
+        hout <- RDHonest::RDOptBW(Ytilde~X,cutoff=c,M=My_tilde,kern=kernel,opt.criterion="FLCI",bw.equal=TRUE,alpha=1-niveau,sclass=sclass,order=1)
+        h <- hout$h[1]
+      }
     }
   }
-  
+
   ## Estimation
   if(length(sig_cov)==0) {
-    RDfit <- rdrobust::rdrobust(Y,X,c=c,h=h$bws[1],b=h$bws[1],kernel=kernel)
+    if(rd=="robust") {
+      RDfit <- rdrobust::rdrobust(Y,X,c=c,h=h,b=h,kernel=kernel,level=niveau*100)
+    } else {
+      RDfit <- RDHonest::RDHonest(Y~X,cutoff=c,M=My,kern=kernel,opt.criterion="FLCI",h=h,alpha=1-niveau,sclass=sclass,order=1)
+    }
   } else {
-    RDfit <- rdrobust::rdrobust(Y,X,c=c,h=h$bws[1],b=h$bws[1],covs=Z[,sig_cov],kernel=kernel)
+    if(rd=="robust") {
+      RDfit <- rdrobust::rdrobust(Y,X,c=c,h=h,b=h,covs=Z[,sig_cov],kernel=kernel,level=niveau*100)
+    } else {
+      RDfit <- RDHonest::RDHonest(Ytilde~X,cutoff=c,M=My,kern=kernel,opt.criterion="FLCI",h=h,alpha=1-niveau,sclass=sclass,order=1)
+    }
   }
-  
+
   return(list(rd=RDfit,covs=sig_cov,b=b,h=h,Zpars=Z_pars))
 }
 
 #' Compute Interaction Terms
-#' 
-#' 
+#'
+#'
 #' \code{interaction_terms} computes all interaction terms of the covariates,
 #' that is, the products of columns, provided in z.
-#' 
-#' 
+#'
+#'
 #' @param z n x p matrix, each column correpsonds to one covariate
 #' @return Matrix with n rows and each columns, corresponds to one interaction,
 #'   i.e., z\[,i\]*z\[,j\] for i,j=1,...,p and i<j. The column name gives the exact
 #'   variables which were interacted.
-#'   
+#'
 #'
 #' @section Authors:
 #' Alexander Kreiss, LSE, United Kingdom. \email{a.kreiss@@lse.ac.uk}
-#' 
+#'
 #' Christoph Rothe, University of Mannheim, Germany
-#' 
+#'
 #' @seealso \code{\link{fourier_basis}}, \code{\link{HighDim_rd}},
-#'   \code{\link{cross_interactions}}   
-#'       
+#'   \code{\link{cross_interactions}}
+#'
 #' @export
 interaction_terms <- function(z) {
   p <- dim(z)[2]
   if(p==1) {stop("Input is one-dimensional, interactions make no sense.\n")}
-  
+
   out <- matrix(NA,nrow=dim(z)[1],ncol=p*(p-1)/2)
   colnames(out) <- rep("a",p*(p-1)/2)
   col_count <- 1
@@ -251,23 +283,23 @@ interaction_terms <- function(z) {
 }
 
 #' Compute Cross-Interactions
-#' 
-#' 
+#'
+#'
 #' \code{cross_interactions} computes all cross interactions, that is, products
 #' of columns, between the matrices z1 nad z2.
-#' 
-#' 
+#'
+#'
 #' @param z1 n x p1 dimensional matrix, each column corresponds to one covariate.
 #' @param z2 n x p2 dimensional maitrx, each column correpsonds to one covariate.
-#' 
-#' 
+#'
+#'
 #' @return n x (p1*p2) Matrix which contains the cross interactions
 #'   z1\[,i\]*z2\[,j\] for i=1,...,p1 and j=1,...,p2. The column name gives involved
 #'   covariates.
 #'
 #' @section Authors:
 #' Alexander Kreiss, LSE, United Kingdom. \email{a.kreiss@@lse.ac.uk}
-#' 
+#'
 #' Christoph Rothe, University of Mannheim, Germany
 #'
 #'
@@ -279,7 +311,7 @@ cross_interactions <- function(z1,z2,ident="") {
   p1 <- dim(z1)[2]
   p2 <- dim(z2)[2]
   n <- dim(z1)[1]
-  
+
   out <- matrix(NA,nrow=n,ncol=p1*p2)
   colnames(out) <- rep("a",p1*p2)
   col_count <- 1
@@ -290,41 +322,41 @@ cross_interactions <- function(z1,z2,ident="") {
       col_count <- col_count+1
     }
   }
-  
+
   return(out)
 }
 
 
 #' Compute Fourier Basis Expansions
-#' 
-#' 
+#'
+#'
 #' \code{fourier_basis} computes Fourier Bases expansions for the covariates in z up to a given order.
 #'
 #'
 #' @param z n x p Matrix, each column corresponds to one covariate.
 #' @param order The order until which the Fourier expansion shall be computed.
-#' 
-#' 
+#'
+#'
 #' @return n x (2 x p x order) Matrix, each column contains an expression of the
 #'   form sin(2*pi*r*z[,i]) or cos(2*pi*r*z[,i]) for r=1,...,order and
 #'   j=1,...,p. The column names are of the form "FB j sin r" or "FB j cos r".
-#'   
+#'
 #' @section Authors:
 #' Alexander Kreiss, LSE, United Kingdom. \email{a.kreiss@@lse.ac.uk}
-#' 
+#'
 #' Christoph Rothe, University of Mannheim, Germany
-#' 
-#' 
+#'
+#'
 #' @seealso \code{\link{HighDim_rd}}, \code{\link{interaction_terms}},
 #'   \code{\link{cross_interactions}}
-#' 
-#'    
+#'
+#'
 #' @export
 fourier_basis <- function(z,order) {
   p <- dim(z)[2]
-  
+
   out <- matrix(NA,nrow=dim(z)[1],ncol=p*2*order)
-  
+
   colnames(out) <- rep("a",p*2*order)
   col_count <- 1
   for(i in 1:p) {
